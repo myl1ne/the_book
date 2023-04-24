9542d63985b87663ad378aaac4387780cbd9899f
Myline <stephane.lallee@gmail.com>
2023-04-25 01:00:50 +0200

    Character on screen, moving, reading text on the book

--- The Book - Web/static/js/common/main_frame.js
+++ The Book - Web/static/js/common/main_frame.js
@@ -4,8 +4,8 @@ import { UnityCanvas } from './unity_canvas.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     setupHeaderFooter();
-    setupNavBarButtons();
     setupUnity();
+    setupNavBarButtons();
 });
 
 function setupUnity() {
@@ -16,8 +16,26 @@ function setupUnity() {
 
     window.addEventListener("WorldEventOnCreated", (e) => {
         console.log("World is loaded and ready to be used");
-            console.log("Spawning a cursor");
-            document.unityCanvas.forwardCursorToUnity();
+        document.unityCanvas.forwardCursorToUnity();
+        
+        document.unityCanvas.nativeBridge.world_CreateOrUpdateEntity(
+            "NavigationTarget",
+            "Generic3D",
+            "ThreeD",
+            { x: 0.0, y: 0.0, z: 0.0 },
+            { x: 0.25, y: 0.25, z: 0.25 },
+            { x: 0.0, y: 0.0, z: 0.0 },
+        );
+        //Making Myline the right size and out of scren
+        document.unityCanvas.nativeBridge.world_CreateOrUpdateEntity(
+            "Myline",
+            null,
+            "ThreeD",
+            { x: 0.0, y: 0.0, z: 0.0 },
+            { x: 2.5, y: 2.5, z: 2.5 },
+            { x: 0.0, y: 0.0, z: 180.0 },
+        );
+        document.unityCanvas.nativeBridge.entity_MoveTo_Target("Myline", "NavigationTarget");
     });
 }
 
@@ -25,67 +43,56 @@ function setupNavBarButtons() {
     const button = document.getElementById('main-frame-btn-speak');
     button.addEventListener('click', async () => {
         button.disabled = true;
-        const response = await post_to_authenticated_route(`/api/say`, {'text': 'Hello World!', 'voice': 'jaina', 'language': 'en'});
-        if (response.ok) {
-            const data = await response.json();
-            console.log("Server response:", data);
-            document.unityCanvas.nativeBridge.entity_Play_Sound("Myline", data.url);
-        } else {
-            console.error("Error:", response.statusText);
-            return false;
+        document.unityCanvas.nativeBridge.entity_Say("Myline", "This is a test.", "jaina", "en" );
+    });
+}
+
+function headerFooterClose() {
+    document.getElementById('body-content').className = 'collapsed-both';
+    let nav = document.querySelector('.nav-drawer');
+    nav.style.display = 'none';
+    nav.parentElement.classList.add('collapsed');
+    let foot = document.querySelector('.footer-drawer');
+    foot.style.display = 'none';
+    foot.parentElement.classList.add('collapsed');
+    const toggleButtons = document.querySelectorAll('.toggle-button');
+    toggleButtons.forEach((btn) => {
+        if (btn.parentElement.tagName === 'HEADER') {
+            btn.querySelector('i').className = 'fas fa-chevron-down';
+        } else if (btn.parentElement.tagName === 'FOOTER') {
+            btn.querySelector('i').className = 'fas fa-chevron-up';
         }
-        button.disabled = false;
+    });
+}
+
+function headerFooterOpen() {
+    document.getElementById('body-content').className = '';
+    let nav = document.querySelector('.nav-drawer');
+    nav.style.display = 'flex';
+    nav.parentElement.classList.remove('collapsed');
+    let foot = document.querySelector('.footer-drawer');
+    foot.style.display = 'flex';
+    foot.parentElement.classList.remove('collapsed');
+    const toggleButtons = document.querySelectorAll('.toggle-button');
+    toggleButtons.forEach((btn) => {
+        if (btn.parentElement.tagName === 'HEADER') {
+            btn.querySelector('i').className = 'fas fa-chevron-up';
+        } else if (btn.parentElement.tagName === 'FOOTER') {
+            btn.querySelector('i').className = 'fas fa-chevron-down';
+        }
     });
 }
 
 function setupHeaderFooter() {
-    function headerFooterClose() {
-        document.getElementById('body-content').className = 'collapsed-both';
-        let nav = document.querySelector('.nav-drawer');
-        nav.style.display = 'none';
-        nav.parentElement.classList.add('collapsed');
-        let foot = document.querySelector('.footer-drawer');
-        foot.style.display = 'none';
-        foot.parentElement.classList.add('collapsed');
-        const toggleButtons = document.querySelectorAll('.toggle-button');
-        toggleButtons.forEach((btn) => {
-            if (btn.parentElement.tagName === 'HEADER') {
-                btn.querySelector('i').className = 'fas fa-chevron-down';
-            } else if (btn.parentElement.tagName === 'FOOTER') {
-                btn.querySelector('i').className = 'fas fa-chevron-up';
+    const toggleButtons = document.querySelectorAll('.toggle-button');
+    toggleButtons.forEach((btn) => {
+        btn.addEventListener('click', function() {
+
+            if (btn.parentElement.classList.contains('collapsed')){
+                headerFooterOpen();
+            } else {
+                headerFooterClose();
             }
-        });
-    }
-
-    function headerFooterOpen() {
-        document.getElementById('body-content').className = '';
-        let nav = document.querySelector('.nav-drawer');
-        nav.style.display = 'flex';
-        nav.parentElement.classList.remove('collapsed');
-        let foot = document.querySelector('.footer-drawer');
-        foot.style.display = 'flex';
-        foot.parentElement.classList.remove('collapsed');
-        const toggleButtons = document.querySelectorAll('.toggle-button');
-        toggleButtons.forEach((btn) => {
-            if (btn.parentElement.tagName === 'HEADER') {
-                btn.querySelector('i').className = 'fas fa-chevron-up';
-            } else if (btn.parentElement.tagName === 'FOOTER') {
-                btn.querySelector('i').className = 'fas fa-chevron-down';
-            }
-        });
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const toggleButtons = document.querySelectorAll('.toggle-button');
-        toggleButtons.forEach((btn) => {
-            btn.addEventListener('click', function() {
-
-                if (btn.parentElement.classList.contains('collapsed')){
-                    headerFooterOpen();
-                } else {
-                    headerFooterClose();
-                }
-            });
         });
     });
 }
